45e7722adcec31d63ea2538628c6e5c362bdb763
User <User@DESKTOP-R9PG6EU>
2022-04-26 15:31:01 +0530

    Initial Commit

---
+++ test-automation-dashboard-rest-api/src/main/java/com/company/dashboard/TestAutomationDashboardRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.company.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAutomationDashboardRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAutomationDashboardRestApiApplication.class, args);
+	}
+
+}
